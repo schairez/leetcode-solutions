79ea0b8b40f5bc61e4e4b1bc5ec9d67b2abb63ee
schairez <34615630+schairez@users.noreply.github.com>
2020-04-17 03:08:16 -0700

    palindrome submitted

---
+++ python-solutions/palindrome/palindrome.py
@@ -0,0 +1,21 @@
+
+"""
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+EX:
+f("madam") yield True since reverse("madam") == "madam"
+"""
+
+
+def invert_str(s: str) -> str:
+    return s[::-1]
+
+
+def is_palindrome(s: str) -> bool:
+    return s == invert_str(s)
+
+
+if __name__ == "__main__":
+    print(is_palindrome("madam"))
--- python-solutions/two-sum/two_sum.py
+++ python-solutions/two-sum/two_sum.py
@@ -24,24 +24,37 @@ complement - the number to add to make the target
 EX: X + Y = target, then Y is X's complement to meet target
 
 X = target - Y 
+[2, 7, 11, 15], t =13
 
+iter:
+comp = 13 -2 = 11
+store: d[comp] = idx ; d[11] = 0
+
+comp = 13 - 7 = 6
+store: d[comp] = idx; d[6] = 1 
+
+comp = 13 - 11 = 2 
+store: d[comp] = idx; d[2] = 2
 ********************************************************************************** 
 
 """
-
+# algo explained:
+# iterate through the elems in nums_array
+# use the elem as the dict_key  --- `complement_key`
 # use the number as the key as well as index as value
 
 
 def two_sum(nums_array: List[int], target: int) -> List[int]:
     complements_d = dict()
     for idx, elem in enumerate(nums_array):
-        complement = target - elem
-        if elem in complements_d:
+        complement_key = target - elem
+        if elem in complements_d.keys():
+            print(complements_d)
             return [complements_d[elem], idx]
-        complements_d[complement] = idx
+        complements_d[complement_key] = idx
 
 
 if __name__ == "__main__":
     nums = [2, 7, 11, 15]
-    target = 9
-    print(two_sum(nums, target))
+    target = 13
+    print(two_sum(nums, target))  # [0, 2]
